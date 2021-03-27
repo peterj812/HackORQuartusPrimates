7e5c8ce92fcbbc97c8b754928711bec3a35f1270
ericksonl <ericksonl@sou.edu>
2021-03-26 19:11:45 -0700

    added random function

--- HackORQuartusPrimates/src/Main.java
+++ HackORQuartusPrimates/src/Main.java
@@ -1,31 +1,33 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.Random;
 
 public class Main {
-	
-	
+	private static Random rand = new Random();
+
 	// METHODS
-	
+
 	void trip() {
 		int min_damage = 0;
 		int max_damage = 3;
 		injury(min_damage, max_damage);
 	}
-	
+
 	void crash() {
 		int min_damage = 3;
 		int max_damage = 10;
 		injury(min_damage, max_damage);
 	}
-	
+
 	// TODO: finish this method please :)
 	void injury(int min_damage, int max_damage) {
 		int damage = 0; // determine the damage using min_damage and max_damage inclusive
 		p.setHealth(p.getHealth() - damage);
 	}
-	
+
 	static void initialize() {
+
 		// these should be between 0 and 100 inclusive
 		double hasAdultItemChance = 25;
 		double hasBandagesChance = 25;
@@ -33,16 +35,16 @@ public class Main {
 		double hasSmallEngine = 25;
 		double hasWaterBottleChance = 25;
 		double tripChance = 25;
-		
+
 		Player p = new Player();
-		
+
 		// ITEMS
 		Item adultItem = new Item("adult item", true);
 		Item bandages = new Item("bandages", false);
 		Item knife = new Item("knife", true);
 		Item smallEngine = new Item("small engine", true);
 		Item waterBottle = new Item("water bottle", true);
-		
+
 		// PANELS
 		Panel init = new Panel("TSA Checkpoint", "");
 		Panel a = new Panel("", "");
@@ -58,54 +60,63 @@ public class Main {
 		Panel k = new Panel("", "");
 		Panel l = new Panel("", "");
 		Panel m = new Panel("", "");
-		
+
 		// SET CHOICES
-		
+
 		// init choices
 		init.setChoice("Look through bag", a);
 		init.setChoice("Go through metal detector", b);
 		init.setChoice("Go home, flying is scary", c);
-		
+
 		// a choices
 		a.setChoice("Throw away contraband", d);
 		a.setChoice("Put bag down and go through security", e);
-		
+
 		// b choices
 		// TODO: implement b outcome (see spreadsheet)
-		
+
 		// c choices
 		// TODO: implement c outcome (see spreadsheet)
-		
+
 		// d choices
-		
+
 		// e choices
-		
+
 		// f choices
-		
+
 		// g choices
-		
+
 		// h choices
-		
+
 		// i choices
-		
+
 		// j choices
-		
+
 		// m choices
-		
+
 		// determine what items will be in backpack
+
 	}
-	
+
+	static void RandomItem(Item in) {
+		int chance = 100;
+		int val = rand.nextInt(chance);
+		if (val <= 25) {
+			p.addItem(in);
+		}
+	}
+
 	public static void main(String[] args) {
 		initialize();
-		
+
 		// set the player name
-		
+
 		// airport sequence
-		
+
 		// plane sequence
-		
+
 		// crash sequence
-		
+
 		// ???
 	}
 
